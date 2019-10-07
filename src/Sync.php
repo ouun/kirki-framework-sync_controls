aebd90dcaca43b8e8aa1f61c35f71b7653ab1d86
Philipp Wellmer <philipp@wellmer.com>
2019-10-07 17:54:44 +0200

    Init commit: Overwrites the foundation of kirki-framework/module-tooltips used as starting point

---
+++ src/Sync.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Syncs Customizer controls until user overwrites the master value.
+ * This is based on the structure that Kirki Modules by Ari Stathopoulos (@aristath) uses
+ *
+ * @package   Kirki
+ * @category  Modules
+ * @author    Philipp Wellmer (@ouun)
+ * @copyright Copyright (c) 2019, Philipp Wellmer (@ouun)
+ * @license   https://opensource.org/licenses/MIT
+ * @since     1.0
+ */
+
+namespace Kirki\Module;
+
+use Kirki\URL;
+
+/**
+ * Adds the tooltips.
+ *
+ * @since 1.0
+ */
+class Sync {
+
+	/**
+	 * An array containing field identifieds and their tooltips.
+	 *
+	 * @access private
+	 * @since 1.0
+	 * @var array
+	 */
+	private $tooltips_content = [];
+
+	/**
+	 * The class constructor
+	 *
+	 * @access public
+	 * @since 1.0
+	 */
+	public function __construct() {
+		add_action( 'customize_controls_enqueue_scripts', [ $this, 'customize_controls_enqueue_scripts' ] );
+		add_filter( 'kirki_field_add_control_args', [ $this, 'filter_control_args' ], 10, 2 );
+	}
+
+	/**
+	 * Enqueue scripts.
+	 *
+	 * @access public
+	 * @since 1.0
+	 */
+	public function customize_controls_enqueue_scripts() {
+		wp_enqueue_script(
+			'kirki-sync', URL::get_from_path( __DIR__ . '/script.js'),
+			array(
+				'customize-controls',
+				'customize-base',
+				'wp-element',
+				'wp-compose',
+				'wp-components',
+				'jquery',
+				'wp-i18n',
+			),
+			'1.0.0',
+			true
+		);
+		wp_enqueue_style( 'kirki-tooltip', URL::get_from_path( __DIR__ . '/styles.css' ), [], '4.0' );
+	}
+
+	/**
+	 * Filter control args.
+	 *
+	 * @access public
+	 * @since 1.0
+	 * @param array                $args         The field arguments.
+	 * @param WP_Customize_Manager $wp_customize The customizer instance.
+	 * @return array
+	 */
+	public function filter_control_args( $args, $wp_customize ) {
+		// Fail early. Proceed only with subcontrols
+		if ( ! isset( $args['parent_setting'] ) ) {
+			return $args;
+		}
+
+		// Mainly for Typography fields
+		if ( isset( $args['parent_setting'] ) && isset( $args['input_attrs'] ) && isset( $args['wrapper_atts'] ) ) {
+			$subcontrol = $args['wrapper_atts']['kirki-typography-subcontrol-type'];
+
+			$args['input_attrs'] = isset( $args['input_attrs'][ $subcontrol ] ) ? $args['input_attrs'][ $subcontrol ] : $args['input_attrs'];
+		}
+
+		return $args;
+	}
+}
--- src/script.js
+++ src/script.js
@@ -1,57 +1,105 @@
-/* global kirkiTooltips */
-function kirkiTooltipAdd( control ) {
-	_.each( kirkiTooltips, function( tooltip ) {
-
-		if ( tooltip.id !== control.id ) {
-			return;
+( function( $, api ) {
+	wp.customize.bind( 'ready', function() {
+  
+	  let syncedControls = [];
+  
+	  // Collect Masters with their Slaves to loop through them
+	  api.control.each( function ( control ) {
+		if ( control.params.inputAttrs !== undefined ) {
+		  let controlHasMaster = control.params.inputAttrs.toString().match(/data-sync-master="(.*?)"/);
+  
+		  if ( controlHasMaster ){
+			let controlMasterID = controlHasMaster[0].match(/"(.*?)"/)[0].replace(/['"]+/g, '');
+			// console.log( 'Sync ' + control.id + ' with ' + controlMasterID );
+  
+			syncedControls.push({
+			  masterID: controlMasterID,
+			  slaveID: control.id,
+			});
+		  }
 		}
-
-		if ( control.container.find( '.tooltip-content' ).length ) {
-			return;
-		}
-
-		// The control ID. Will be used to manipulate the DOM.
-		const controlID = '#customize-control-' + tooltip.id;
-
-		// The trigger markup.
-		const trigger = '<span class="tooltip-trigger" data-setting="' + tooltip.id + '"><span class="dashicons dashicons-editor-help"></span></span>';
-
-		// Build the tooltip content.
-		const content = '<div class="tooltip-content hidden" data-setting="' + tooltip.id + '">' + tooltip.content + '</div>';
-
-		// Add the trigger & content.
-		jQuery( '<div class="tooltip-wrapper">' + trigger + content + '</div>' ).prependTo( controlID );
-
-		// Handle onclick events.
-		jQuery( '.tooltip-trigger[data-setting="' + tooltip.id + '"]' ).on( 'click', function() {
-			jQuery( '.tooltip-content[data-setting="' + tooltip.id + '"]' ).toggleClass( 'hidden' );
-		} );
-	} );
-}
-
-jQuery( document ).ready( function() {
-
-	// Close tooltips if we click anywhere else.
-	jQuery( document ).mouseup( function( e ) {
-
-		if ( ! jQuery( '.tooltip-content' ).is( e.target ) ) {
-			if ( ! jQuery( '.tooltip-content' ).hasClass( 'hidden' ) ) {
-				jQuery( '.tooltip-content' ).addClass( 'hidden' );
-			}
-		}
-	} );
-
-	wp.customize.control.each( function( control ) {
-		wp.customize.section( control.section(), function( section ) {
-			if ( section.expanded() || wp.customize.settings.autofocus.control === control.id ) {
-				kirkiTooltipAdd( control );
+	  });
+  
+	  // Loop through Masters with Slaves object to set-up syncs for each slave
+	  $.each( syncedControls, function( index, sync ) {
+		api( sync.masterID, sync.slaveID, function( master, slave ) {
+		  let slaveItem           = wp.customize.control( slave.id ),
+			  slaveLabel          = slaveItem.params.label,
+			  slaveDescription    = slaveItem.params.description,
+			  slaveValue          = slave.instance(),
+			  masterValue         = master.instance();
+  
+		  // Append "Sync-Indicator" link, which focus master control
+		  // todo: Temporary fix until Kirki uses more React
+		  if ( slaveItem.params.choices.type !== 'hidden' ) {
+			let append = '<sup class="sync-indicator"><small><a onclick="wp.customize.control(\'' + master.id + '\').focus();"> AUTO </a></small></sup>';
+  
+			if( $( slaveItem.selector + ' label' ).length ) {
+			  // Works for react field
+			  $( slaveItem.selector + ' label' ).append(append);
 			} else {
-				section.expanded.bind( function( expanded ) {
-					if ( expanded ) {
-						kirkiTooltipAdd( control );
-					}
-				} );
+			  // Kirki Backwards compatibility
+			  if ( slaveLabel !== '' ) {
+				slaveItem.params.label = slaveLabel + append;
+			  } else if ( slaveDescription !== '' ) {
+				slaveItem.params.description = slaveDescription + append;
+			  }
 			}
-		} );
-	} );
-} );
+		  }
+  
+		  // Set overwrite if slave is empty
+		  let overwrite = slave.instance() === '';
+  
+		  // Initially set slave value to master default if empty
+		  if ( slave.instance() === '' ) {
+			if (overwrite) slave.set( master.instance() );
+		  }
+  
+		  slave.bind( function() {
+			// Keep fields synced (Overwrite) if slave is empty
+			overwrite = slave.instance() === '';
+			// Keep fields synced if same values
+			if ( slave.instance() === master.instance() ) overwrite = true;
+			// Unlink slave from master if it is customized
+			if ( !overwrite ) slave.unlink( master );
+			// Reset to master when resetting field
+			if ( slave.instance() === '' ) {
+			  slave.set( master.instance() );
+			  overwrite = true;
+			}
+			// Update to new value
+			slaveValue = slave.instance();
+			// console.log('SLAVE: ' + overwrite + ' ' + slave.instance());
+  
+			updateSlave( overwrite, slaveItem );
+		  });
+  
+		  master.bind( function() {
+			// Keep fields synced if same values (check against old value)
+			if ( masterValue === slaveValue ) overwrite = true;
+			// Link slave with master if overwrite
+			if ( overwrite ) slave.link( master );
+			// Update to new value
+			masterValue = master.instance();
+			//console.log('CHANGE: ' + overwrite + ' ' + master.instance());
+  
+			// Let's update the slaves
+			updateSlave( overwrite, slaveItem );
+		  });
+  
+		  function updateSlave( overwrite, slaveItem ) {
+			if ( overwrite ) {
+			  // Switch Sync-Indicator text
+			  $(slaveItem.selector).find('.sync-indicator a').text( ' AUTO ' );
+			  // If it is a color field, update all Slaves with Master value!
+			  $(slaveItem.selector).find('.wp-color-picker').wpColorPicker('color', masterValue);
+			} else {
+			  $(slaveItem.selector).find('.sync-indicator a').text( ' CUSTOM ' );
+			}
+		  }
+  
+		});
+	  });
+	});
+  } )( jQuery, wp.customize );
+  
